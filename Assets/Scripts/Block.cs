64ef43d73e6148dfde275fca6f6c8d1b0578c315
mogit <mochi.warabii@gmail.com>
2023-02-18 17:32:28 +0900

    first

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public int block_hp; //ブロックHP
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            block_hp--;
+
+            if (block_hp == 0)
+            {
+                //アイテムを1/5の確立で生成
+                if (Random.Range(0, 1) == 0)
+                {
+                    GameManager.instance.ItemGenerate();
+                }
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
